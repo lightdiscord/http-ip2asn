835d480fd7c89ba032635c282165bbddbd970e62
LightDiscord <root@arnaud.sh>
2025-06-12 18:58:57 +0200

    feat: database synchronization

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 #![feature(btree_cursors)]
 
+use crate::DatabaseError::{
+    MissingASDescription, MissingASNumber, MissingCountryCode, MissingRangeEnd, MissingRangeStart,
+};
+use arc_swap::ArcSwapOption;
 use axum::Router;
 use axum::extract::{ConnectInfo, State};
 use axum::http::{HeaderMap, StatusCode};
 use axum::routing::get;
 use clap::Parser;
 use flate2::read::GzDecoder;
+use log::{debug, error, info};
+use reqwest::Url;
 use std::collections::{BTreeMap, Bound};
 use std::io::Read;
 use std::net::{IpAddr, SocketAddr};
 use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Arc;
+use std::time::Duration;
+use thiserror::Error;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
-use log::info;
-use reqwest::Url;
+use tokio::time::sleep;
+use tokio::{task, try_join};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -25,12 +33,16 @@ struct Cli {
     database_file: Option<PathBuf>,
 
     /// Download URL for the database file to be fetched regularly.
-    #[arg(long, env = "IPTOASN_DATABASE_URL", default_value = "https://iptoasn.com/data/ip2asn-combined.tsv.gz")]
+    #[arg(
+        long,
+        env = "IPTOASN_DATABASE_URL",
+        default_value = "https://iptoasn.com/data/ip2asn-combined.tsv.gz"
+    )]
     database_url: Url,
 
     /// Database retrieval frequency in seconds
     #[arg(long, env = "IPTOASN_DATABASE_FREQUENCY", default_value = "3600")]
-    database_frequency: usize,
+    database_frequency: u64,
 
     /// Host used for the web server
     #[arg(long, env = "HOST", default_value = "0.0.0.0")]
@@ -41,15 +53,8 @@ struct Cli {
     port: u16,
 }
 
-fn gunzip(bytes: Vec<u8>) -> std::io::Result<String> {
-    let mut gz = GzDecoder::new(&bytes[..]);
-    let mut s = String::new();
-    gz.read_to_string(&mut s)?;
-    Ok(s)
-}
-
 #[derive(Debug)]
-struct ASN {
+struct Asn {
     range_start: IpAddr,
     range_end: IpAddr,
     as_number: u32,
@@ -58,16 +63,17 @@ struct ASN {
 }
 
 struct Database {
-    inner: BTreeMap<IpAddr, ASN>,
+    inner: BTreeMap<IpAddr, Asn>,
 }
 
 impl Database {
-    fn new(inner: BTreeMap<IpAddr, ASN>) -> Self {
+    fn new(inner: BTreeMap<IpAddr, Asn>) -> Self {
         Self { inner }
     }
 
-    pub fn get(&self, address: IpAddr) -> Option<&ASN> {
-        self.inner.upper_bound(Bound::Included(&address))
+    pub fn get(&self, address: IpAddr) -> Option<&Asn> {
+        self.inner
+            .upper_bound(Bound::Included(&address))
             .peek_prev()
             .map(|(_index, asn)| asn)
             .filter(|asn| asn.range_start <= address && address <= asn.range_end)
@@ -75,23 +81,58 @@ impl Database {
 }
 
 struct AppState {
-    database: Option<Database>,
+    options: Cli,
+    database: ArcSwapOption<Database>,
+}
+
+#[derive(Error, Debug)]
+pub enum DatabaseError {
+    #[error("IO error")]
+    IoError(#[from] std::io::Error),
+
+    #[error("Invalid IP address")]
+    AddrParseError(#[from] std::net::AddrParseError),
+
+    #[error("Parse integer error")]
+    ParseIntError(#[from] std::num::ParseIntError),
+
+    #[error("Missing range start")]
+    MissingRangeStart,
+
+    #[error("Missing range end")]
+    MissingRangeEnd,
+
+    #[error("Missing AS number")]
+    MissingASNumber,
+
+    #[error("Missing country code")]
+    MissingCountryCode,
+
+    #[error("Missing AS description")]
+    MissingASDescription,
+}
+
+fn gunzip(bytes: Vec<u8>) -> Result<String, DatabaseError> {
+    let mut gz = GzDecoder::new(&bytes[..]);
+    let mut s = String::new();
+    gz.read_to_string(&mut s)?;
+    Ok(s)
 }
 
 // TODO: Change error
-fn load_asns(contents: String) -> Result<Database, Box<dyn std::error::Error>> {
+fn load_asns(contents: String) -> Result<Database, DatabaseError> {
     let mut map = BTreeMap::new();
 
     for line in contents.lines() {
         let mut parts = line.splitn(5, '\t');
 
-        let range_start: IpAddr = parts.next().ok_or("Missing range start")?.parse()?;
-        let range_end: IpAddr = parts.next().ok_or("Missing range end")?.parse()?;
-        let as_number: u32 = parts.next().ok_or("Missing AS number")?.parse()?;
-        let country_code = parts.next().ok_or("Missing country code")?;
-        let description = parts.next().ok_or("Missing AS description")?;
+        let range_start: IpAddr = parts.next().ok_or(MissingRangeStart)?.parse()?;
+        let range_end: IpAddr = parts.next().ok_or(MissingRangeEnd)?.parse()?;
+        let as_number: u32 = parts.next().ok_or(MissingASNumber)?.parse()?;
+        let country_code = parts.next().ok_or(MissingCountryCode)?;
+        let description = parts.next().ok_or(MissingASDescription)?;
 
-        let asn = ASN {
+        let asn = Asn {
             range_start,
             range_end,
             as_number,
@@ -105,34 +146,85 @@ fn load_asns(contents: String) -> Result<Database, Box<dyn std::error::Error>> {
     Ok(Database::new(map))
 }
 
+async fn database_synchronization_once(
+    options: &Cli,
+) -> Result<Database, Box<dyn std::error::Error>> {
+    let database = if let Some(path) = &options.database_file {
+        let mut file = File::open(path).await?;
+        let mut contents = vec![];
+        file.read_to_end(&mut contents).await?;
+
+        contents
+    } else {
+        unimplemented!("download from address")
+    };
+
+    let database = task::spawn_blocking(move || {
+        let database = gunzip(database);
+
+        match database {
+            Ok(database) => load_asns(database),
+            Err(error) => Err(error),
+        }
+    })
+    .await?;
+
+    Ok(database?)
+}
+
+async fn database_synchronization(state: Arc<AppState>) -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        //sleep(Duration::from_secs(10)).await;
+        debug!("Starting database synchronization");
+
+        match database_synchronization_once(&state.options).await {
+            Ok(database) => {
+                state.database.store(Some(Arc::new(database)));
+
+                info!("Database has been synchronized");
+
+                sleep(Duration::from_secs(state.options.database_frequency)).await;
+            }
+            Err(e) => {
+                error!("error while synchronizing database: {}", e);
+
+                // TODO: Exponential back-off.
+            }
+        }
+    }
+}
+
+async fn webserver(state: Arc<AppState>) -> Result<(), Box<dyn std::error::Error>> {
+    let listen_address = SocketAddr::new(state.options.host, state.options.port);
+    let app = Router::new()
+        .route("/", get(root))
+        .with_state(state.clone())
+        .into_make_service_with_connect_info::<SocketAddr>();
+
+    let listener = tokio::net::TcpListener::bind(listen_address).await?;
+    info!("Server listening {}", listen_address);
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
     let cli = Cli::parse();
 
-    let mut file = File::open(cli.database_file.unwrap()).await?;
-    let mut contents = vec![];
-    file.read_to_end(&mut contents).await?;
+    // if cli.database_file.is_so
+    //
 
-    info!("Parsing database file");
+    let state = Arc::new(AppState {
+        options: cli,
+        database: ArcSwapOption::from(None),
+    });
 
-    let contents = gunzip(contents)?;
-
-    let state = AppState {
-        database: Some(load_asns(contents)?),
-    };
-
-    let state = Arc::new(state);
-
-    let app = Router::new()
-        .route("/", get(root))
-        .with_state(state)
-        .into_make_service_with_connect_info::<SocketAddr>();
-
-    let listen_address = SocketAddr::new(cli.host, cli.port);
-    let listener = tokio::net::TcpListener::bind(listen_address).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    try_join!(webserver(state.clone()), database_synchronization(state))?;
 
     Ok(())
 }
@@ -142,7 +234,9 @@ async fn root(
     State(state): State<Arc<AppState>>,
     headers: HeaderMap,
 ) -> (StatusCode, HeaderMap) {
-    if let Some(database) = state.database.as_ref() {
+    let database = state.database.load();
+
+    if let Some(database) = &*database {
         let address = headers
             .get("X-Forwarded-For")
             .map(|addr| IpAddr::from_str(addr.to_str().unwrap()))
